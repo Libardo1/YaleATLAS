4565c0b1290ce2c472e3e91dee07f703b1e3f063
Michela Paganini <michela.paganini@cern.ch>
2017-05-26 10:32:15 -0700

    restructure viz

--- setup.py
+++ setup.py
@@ -22,3 +22,11 @@ setup(
     install_requires=['pandas', 'numpy', 'rootpy'],
     packages=find_packages()
 )
+
+setup(
+    name='viz',
+    version='0.0.1',
+    description='Library 4 viz',
+    install_requires=['numpy', 'matplotlib'],
+    packages=find_packages()
+)
